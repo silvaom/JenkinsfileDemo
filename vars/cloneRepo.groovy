c679201e2234de3917faa9e1f3cf71b9a471d53b
Omar Silva <omarsilva.globant@latam.com>
2022-05-06 16:38:37 -0300

    Fix

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -1,9 +1,9 @@
 def call(){
     withEnv([SERVICE_DIRECTORY = "~/apps/jenkins/workspace/"]) {
-    dir(SERVICE_DIRECTORY) {
-    printEnv()
-    echo"Cloning repo..."
-    checkout scm
+        dir(SERVICE_DIRECTORY) {
+        printEnv()
+        echo"Cloning repo..."
+        checkout scm
+            }
         }
-    }
 }

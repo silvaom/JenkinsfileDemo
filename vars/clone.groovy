1884ddc8fd4bfff603a9707c0867ac0d4e4512ff
silva omar <zilvaomar@gmail.com>
2022-05-20 16:03:57 -0300

    Fix

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -2,6 +2,8 @@ def call() {
     sh (
         script: '''#!/bin/bash
           set -euxo pipefail
+          echo "$GITREPO"
+          echo "$BRANCH"
           git status
           git clone "$GITREPO" 
           git checkout "$BRANCH"
